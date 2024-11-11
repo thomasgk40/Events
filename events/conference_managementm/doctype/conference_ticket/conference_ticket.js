c17e31eb78a43156699227413e7f7a5bdf09af2d
thomasgk40 <thomasgeorgek40@gmail.com>
2024-11-11 12:57:05 +0000

    U1

--- events/conference_managementm/doctype/conference_ticket/conference_ticket.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Printechs and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Conference Ticket", {
-// 	refresh(frm) {
-
-// 	},
-// });
